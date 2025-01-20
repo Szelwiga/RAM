515f73dce1f7e432134cfd4c9655883f0d354e64
Szelwiga <szelwigamarcel@wp.pl>
2025-01-20 18:27:11 +0100

    Finish pure simulator; adjust some css; fix style issues

---
+++ js/driver.js
@@ -0,0 +1,13 @@
+function D_req_cache_next_page() {
+	N_notify("Not implemented!");
+}
+
+function D_req_cache_prev_page() {
+	N_notify("Not implemented!");
+}
+
+function D_global_init() {
+	/* TODO */
+	EA_init(1);
+	PS_init();
+}
--- js/pure_simulator.js
+++ js/pure_simulator.js
@@ -4,7 +4,7 @@ function PS_color(text, color) {
 	 return "<span style=\"color:var(" + color + ");display:inline;\">" + text + "</span>";
 }
 
-function PS_counters(mem, time, ins) {
+function PS_set_counters(mem, time, ins) {
 	var text = "<pre>";
 	if (window.innerWidth >= G_min_width_viewport) {
 		text += "Counters<br>"
@@ -24,10 +24,10 @@ function PS_counters(mem, time, ins) {
 		text += line1 + line2 + "</pre>";
 	}
 	text = text.replaceAll("Counters", PS_color("<b>Counters</b>", "--light_purple"));
-	return text;
+	document.getElementById("PS-counters").innerHTML = text;
 }
 
-function PS_alu(value, state, error) {
+function PS_set_alu(value, state, error) {
 	text = "<pre>";
 	if (window.innerWidth >= G_min_width_viewport) {
 		text += "Accumulator<br>";
@@ -52,10 +52,10 @@ function PS_alu(value, state, error) {
 		text += line1 + line2 + "</pre>";
 	}
 	text = text.replaceAll("Accumulator", PS_color("<b>Accumulator</b>", "--light_orange"));
-	return text;
+	document.getElementById("PS-alu").innerHTML = text;
 }
 
-function PS_instruction(instruction, event) {
+function PS_set_instruction(instruction, event) {
 
 	var text = "<pre>";
 	text += "Instruction <br>";
@@ -67,12 +67,41 @@ function PS_instruction(instruction, event) {
 	text += "</pre>";
 
 	text = text.replaceAll("Instruction", PS_color("<b>Instruction</b>", "--light_red"));
-	return text;
+	document.getElementById("PS-instruction").innerHTML = text;
 }
-function PS_clear(){
-	document.getElementById("PS-counters").innerHTML    = PS_counters(0, 0, 0);
-	document.getElementById("PS-alu").innerHTML         = PS_alu(0, "inactive", "");
-	document.getElementById("PS-instruction").innerHTML = PS_instruction("", "");
+
+function PS_set_output(text) {
+	document.getElementById("PS-output").value = text;
+}
+
+function PS_set_memory(mem) {
+	var cell_box = document.getElementById("PS-memory-cells-box");
+	for (var mem_cell of mem) {
+		var cell = document.createElement("PRE");
+
+		if (mem_cell[1] != undefined) cell.innerHTML = mem_cell[0] + ": " + mem_cell[1];
+		else                          cell.innerHTML = mem_cell[0] + ": ?";
+		cell.className = "PS-memory-cell"
+
+		cell_box.appendChild(cell);
+	}
+}
+
+function PS_clear() {
+	PS_set_counters(0, 0, 0);
+	PS_set_alu(0, "inactive", "");
+	PS_set_instruction("", "");
+	PS_set_output("");
+
+	var mem = [];
+	for (var i = 1; i <= RAM_DEFAULT_CACHE_SIZE; i++)
+		mem.push([i, undefined]);
+	PS_set_memory(mem);
+	
+}
+
+function PS_get_input() {
+	return document.getElementById("PS-input").value;
 }
 
 function PS_init(){
@@ -87,15 +116,15 @@ function PS_init(){
 				<div class="PS-instruction" id="PS-instruction"></div>
 			</div>
 			<hr>
-			<div class="PS-memory" id="PS-memory"> </div>
+			<div class="PS-memory" id="PS-memory">
+				<button id="PS-memory-left" onclick="D_req_cache_prev_page()">&lt;</button>
+				<div class="PS-memory-cells-box" id="PS-memory-cells-box"></div>
+				<button id="PS-memory-right" onclick="D_req_cache_next_page()">&gt;</button>
+			</div>
 			<hr>
 			<textarea id="PS-output" placeholder="Program output ..." readonly></textarea>
 		</div>
 	`;
 	PS_is_launched = 1;
 	PS_clear();
-}
-
-function PS_update_state() {
-
 }
